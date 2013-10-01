eb4859bd65a376820419a4f1aeb302ce6b9901d1
Rune Langoy <rune.langoy@gmail.com>
2013-10-01 15:22:54 +0200

    Added the function SplitCommands for parsing a CVS String

--- arduino_src/SerialReadLineExample/SerialReadLineExample.ino
+++ arduino_src/SerialReadLineExample/SerialReadLineExample.ino
@@ -1,9 +1,12 @@
 
 #define MAX_RECV_LINE_LENGTH 200
+#define MAX_COMMANDS 10
 
 String strIncompleteLine = "";   // a string to hold incoming data
-String strInputLine = "";            // a string to the recieved line
+String strInputLine = "";        // a string to the recieved line
 boolean bStrComplete = false;    // whether the string is complete
+
+String strCommands[MAX_COMMANDS];
 
 void setup() {
   // initialize serial:
@@ -13,13 +16,45 @@ void setup() {
   strInputLine.reserve(MAX_RECV_LINE_LENGTH);  
 }
 
-void loop() {
+//  strCommands - input string that contains a CVS string ($LED,1,0,1,0,2)
+//  strTab - has the splited input (strTab[0]="$LED",strTab[1]="1",strTab[2]="0")
+//  maxItems -  the maximum commands to parse
+//  Return the number of read items
+int SplitCommands(String  strCommands, String strTab[],int maxItems)
+{
+  //Start position looking for ','
+  int fisrtCommaPosition=0; 
+  int lastCommaPosition=0;
+  int iItems=0;
+  do
+    { //Get the position of the next item
+        lastCommaPosition = strCommands.indexOf(',',fisrtCommaPosition);
+        if(lastCommaPosition != -1)
+        {   strTab[iItems]= strCommands.substring(fisrtCommaPosition,lastCommaPosition);
+            fisrtCommaPosition=lastCommaPosition+1;             
+        }
+        else
+            //Prints the last element
+            strTab[iItems]= strCommands.substring(fisrtCommaPosition,strInputLine.length());
+      
+       iItems++; 
+
+     } while((lastCommaPosition >=0) & (iItems<maxItems));    
+
+  return iItems;
+}
+
+//Main loop do the work :)
+void loop() 
+{
+  int i=0;
   // print the string when a newline arrives:
-  if (bStrComplete) {
-    Serial.print("Arduino Recieved: " + strInputLine);
-    // clear the string:
-    strInputLine = "";
-    bStrComplete = false;
+  if (bStrComplete) 
+  { bStrComplete=false;
+    // Splits the incoming string into strCommands[0] .. [MAX_COMMANDS]
+    int nCommands=SplitCommands(strInputLine,strCommands,MAX_COMMANDS); // Parses the CVS string
+    for(i=0;i<nCommands;i++)
+      Serial.println(strCommands[i]);  //Display the content of the table  
   }
 }
 
