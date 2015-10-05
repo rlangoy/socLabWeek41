686cbfc6ad1ee62e0ede11bc41e244fcc934cbfc
rlangoy <rune.langoy@gmail.com>
2015-10-05 14:59:57 +0200

    Removed references to Arduino

--- python_src/SerialSendLCDMessage.py
+++ python_src/SerialSendLCDMessage.py
@@ -5,12 +5,12 @@ import time
 
 port = "/dev/ttyACM0"
 
-SerialIOArduino = serial.Serial(port,9600) #setup the serial port and baudrate
-SerialIOArduino.flushInput()                #Remove old input's
+SerialIOmbed = serial.Serial(port,9600) #setup the serial port and baudrate
+SerialIOmbed.flushInput()                #Remove old input's
 
 time.sleep(2)   #Wait 2 sek for the Arduino to reboot before prceeding
 
-SerialIOArduino.write("$LCD,Hello Line 1,Hello Line 2\n")
+SerialIOmbed.write("$LCD,Hello Line 1,Hello Line 2\n")
 
-SerialIOArduino.close()
+SerialIOmbed.close()
 
