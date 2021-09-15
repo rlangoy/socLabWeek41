235a6414ae99935ee34834ae25f86f1042d55624
Rune Langøy <rune.langoy@gmail.com>
2021-09-15 09:38:38 +0100

    Update SerialRecieveAndDecodeCSV.py

--- python_src/SerialRecieveAndDecodeCSV.py
+++ python_src/SerialRecieveAndDecodeCSV.py
@@ -8,6 +8,8 @@ SerialIOmbed.flushInput() #Remove old input's
 
 while True:
     if (SerialIOmbed.inWaiting() > 0):
-        inputLine = SerialIOmbed.readline().strip()  # read a '\n' terminated line()
+        inputLine = SerialIOmbed.readline() # read a '\n' terminated line()
+        inputLine=inputLine.decode("utf-8").strip()   #Convert fro, byte to str and remove \n\r
         comands=inputLine.split(",") # Splits the line into a table/list like : ['$SW', '1', '1', '0', '0']
         print(comands)
+
